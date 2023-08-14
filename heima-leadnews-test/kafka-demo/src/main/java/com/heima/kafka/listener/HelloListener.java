8125ce4d0802dc5534dbad0dee1b5ebe8780804d
Mac <302900320@qq.com>
2023-08-14 14:07:49 +0800

    添加kafka测试demo

---
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/listener/HelloListener.java
@@ -0,0 +1,21 @@
+package com.heima.kafka.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.heima.kafka.pojo.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+public class HelloListener {
+
+    @KafkaListener(topics = "user-topic")
+    public void onMessage(String message){
+        System.out.println("s");
+        if(!StringUtils.isEmpty(message)){
+            User user = JSON.parseObject(message, User.class);
+            System.out.println(user);
+        }
+
+    }
+}
