d8eec441517130afcd5b904d8f955c748e14b64c
Mac <302900320@qq.com>
2023-08-21 22:16:47 +0800

    day7-根据用户输入的关键字展示联想词/api/v1/associate/search，完成day7全部需求

---
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/ApAssociateWordsService.java
@@ -0,0 +1,22 @@
+package com.heima.search.service;
+
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.search.dtos.UserSearchDto;
+
+/**
+ * <p>
+ * 联想词表 服务类
+ * </p>
+ *
+ * @author itheima
+ */
+public interface ApAssociateWordsService {
+
+    /**
+     联想词
+     @param userSearchDto
+     @return
+     */
+    ResponseResult findAssociate(UserSearchDto userSearchDto);
+
+}
